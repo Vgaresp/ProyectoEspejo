4fabd0a801e94ffa2dc43f1ee0cb3e32b5aabbc1
“begimode“ <amurchik1510@mail.ru>
2021-09-29 06:53:46 +0200

    Registrarme

--- app/src/main/java/com/example/appespejo/LoginActivity.java
+++ app/src/main/java/com/example/appespejo/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.appespejo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,21 +10,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    Button signUpButton = findViewById(R.id.signUpButton);
-    EditText textLogin = findViewById(R.id.editTextTextPersonName);
-    EditText textPassword = findViewById(R.id.editTextTextPassword);
+    Context context;
+
     String username = "Admin";
-    String password = "1234";
+    String passwordd = "1234";
     boolean isValue = false;
     int counter = 5;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        context = this;
 
 //        Setup
         setup();
@@ -31,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void setup() {
+
+        Button signUpButton = this.findViewById(R.id.signUpButton);
+        EditText textLogin = this.findViewById(R.id.editTextTextPersonName);
+        EditText textPassword = this.findViewById(R.id.editTextTextPassword);
+        Button register = this.findViewById(R.id.button2);
 
         signUpButton.setOnClickListener(new View.OnClickListener(){
 
@@ -40,24 +47,34 @@ public class MainActivity extends AppCompatActivity {
                 String inputPassword = textPassword.getText().toString();
 
                 if(inputName.isEmpty() || inputPassword.isEmpty()){
-                    Toast.makeText(MainActivity.this,"Please enter all the details correctly",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this,"Please enter all the details correctly",Toast.LENGTH_SHORT).show();
                 }else{
                     isValue = validate(inputName,inputPassword);
 
                     if(!isValue){
                         counter--;
-                        Toast.makeText(MainActivity.this,"Incorrect",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LoginActivity.this,"Incorrect",Toast.LENGTH_SHORT).show();
                         if(counter ==0){
                             signUpButton.setEnabled(false);
                         }
                     }else{
-                        Toast.makeText(MainActivity.this,"Login successful",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LoginActivity.this,"Login successful",Toast.LENGTH_SHORT).show();
+
                         // add the code to go to a new activity
-                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                         startActivity(intent);
                     }
                 }
             }
+        });
+
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+
         });
 
     }
@@ -66,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean validate(String name, String password){
 
-        if(name.equals(username) && password.equals(password)){
+        if(name.equals(username) && password.equals(passwordd)){
             return true;
         }
         return false;
---
+++ app/src/main/java/com/example/appespejo/RegisterActivity.java
@@ -0,0 +1,105 @@
+package com.example.appespejo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private String name = "";
+    private String pass = "";
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDataBase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        setup();
+
+        mAuth = FirebaseAuth.getInstance();
+        mDataBase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    private void setup(){
+
+        Button register = this.findViewById(R.id.registrarmeButton);
+        EditText usuario = this.findViewById(R.id.EditTextRegUsername);
+        EditText contrasena = this.findViewById(R.id.editTextRegContras);
+
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                name = usuario.getText().toString();
+                pass = contrasena.getText().toString();
+
+                if(!name.isEmpty() || !pass.isEmpty()){
+                    if(pass.length() < 6){
+                        Toast.makeText(RegisterActivity.this, "La contrasena debe consistir al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }else{
+                        registerUser();
+                    }
+
+                }else{
+                    Toast.makeText(RegisterActivity.this, "Debe completar los campos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void registerUser(){
+        mAuth.createUserWithEmailAndPassword(name,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                    Map<String,Object> map = new HashMap<>();
+                    map.put("Usuario",name);
+                    map.put("Contrasena",pass);
+
+                    String id = mAuth.getCurrentUser().getUid();
+
+                    mDataBase.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> taskZ) {
+                            if(taskZ.isSuccessful()){
+                                Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                                startActivity(intent);
+//                                finish(); //Para evitar que vuelva a la pantalla del registro
+
+                            }
+                            else{
+                                Toast.makeText(RegisterActivity.this, "Habia un error en crear nuevos datos", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }else{
+                    Toast.makeText(RegisterActivity.this,"No se pudo registrar el usuario",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+}
